58b6808bcc4a95ea0579e6ed64874f403cb081af
ines1024 <inegalarza@gmail.com>
2023-06-14 09:29:39 -0300

    milgred

--- script.js
+++ script.js
@@ -178,8 +178,8 @@ d3.csv('dataset_canciones.csv', d3.autoType).then(data => {
       y: 'energy'})
       
     ],
-    width: 1100,
-      height: 700,
+    width: 2200,
+      height: 900,
       insetLeft: 40,
       insetRight: 40,
       marginBottom: 50,
@@ -202,7 +202,7 @@ d3.csv('dataset_canciones.csv', d3.autoType).then(data => {
       },
       style: {
         fontFamily: 'sans-serif',
-        fontSize: 20,
+        fontSize: 42,
         background: 'white',
         
     
